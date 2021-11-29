1fef7077fd6c87a3ecd486704ffe40f039e96d60
유국현 <rnrgus5897@gmail.com>
2021-11-29 18:28:17 +0900

    antdesign table

--- pages/articles/index.js
+++ pages/articles/index.js
@@ -3,31 +3,31 @@ import Link from "next/link"
 import firebaseApp from "../../net/firebaseApp";
 import {getFirestore , collection , getDocs} from "firebase/firestore/lite"
 import { DateTime } from "luxon";
-import {Button} from "antd"
+import {Button , Table} from "antd"
 import { useEffect, useState } from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEdit , faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 
-function Item({article}){
-    return(
-        <tr className="border" >
-            <th scope="row" className="py-3 px-2">{article.id}</th>
-            <td className="pl-3">
+// function Item({article}){
+//     return(
+//         <tr className="border" >
+//             <th scope="row" className="py-3 px-2">{article.id}</th>
+//             <td className="pl-3">
     
-            <a><FontAwesomeIcon icon={faEdit} className="fas fa-edit" size="lg" /> & </a>
+//             <a><FontAwesomeIcon icon={faEdit} className="fas fa-edit" size="lg" /> & </a>
 
-            <a><FontAwesomeIcon icon={faTrashAlt} className="fas fa-trash-alt" size="lg" /></a>
+//             <a><FontAwesomeIcon icon={faTrashAlt} className="fas fa-trash-alt" size="lg" /></a>
 
-            </td>
-            <td>{article.name}</td>
-            <td>{article.age}</td>
-            <td>{article.live}</td>
-            <td>{article.work}</td>
-            <td>{article.phone}</td>
-            <td>{DateTime.fromSeconds(article.created_at.seconds).toFormat('yyyy-LL-dd')}</td>
-        </tr>
-    )
-}
+//             </td>
+//             <td>{article.name}</td>
+//             <td>{article.age}</td>
+//             <td>{article.live}</td>
+//             <td>{article.work}</td>
+//             <td>{article.phone}</td>
+//             <td>{DateTime.fromSeconds(article.created_at.seconds).toFormat('yyyy-LL-dd')}</td>
+//         </tr>
+//     )
+// }
 
 
 export default function ArticleList(){
@@ -49,14 +49,53 @@ export default function ArticleList(){
         
     }, [])
 
-
+    const columns = [
+        {
+          title: '직원번호',
+          dataIndex: 'id',
+          key: 'id',
+        },
+        {
+          title: '이름',
+          dataIndex: 'name',
+          key: 'name',
+        },
+        {
+          title: '나이',
+          dataIndex: 'age',
+          key: 'age',
+        },
+        {
+          title: '부서',
+          dataIndex: 'work',
+          key: 'work',
+        },
+        {
+          title: '핸드폰번호',
+          dataIndex: 'phone',
+          key: 'phone',
+        },
+        {
+          title: '거주지역',
+          dataIndex: 'live',
+          key: 'live',
+        },
+        {
+          title: '입사일자',
+          dataIndex: 'created_at',
+          key: 'created_at',
+          render : (text , record) => {
+                return DateTime.fromSeconds(record.created_at.seconds).toFormat('yyyy-LL-dd HH:mm')
+          }
+        },
+      ];
 
 
     return(
         <BaseLayout>
             <h1>직원정보 crud</h1>
-          <div className="container">
-            <table className="table container text-justify">
+    
+            {/* <table className="table container text-justify">
                 <thead>
                     <tr className="border">
                         <th scope="col" className="py-3 px-2">직원번호</th>
@@ -74,9 +113,9 @@ export default function ArticleList(){
                 <tbody>
                     {articles.map(article => (<Item key={article.id} article={article} />))}
                 </tbody>
-                </table>
-            </div>
-           
+                </table> */}
+            {console.log("articles :::" , articles)}
+            <Table dataSource={articles} columns={columns} rowKey={'id'} />;
 
 
             <div className="flex flex-row justify-end my-6">
